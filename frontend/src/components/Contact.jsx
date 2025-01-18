d6ebccf5bf5f3a43590af160a1a75f8cae394add
AnishInGit <123265800+AnishInGit@users.noreply.github.com>
2025-01-18 13:59:46 +0530

    Update

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -200,12 +200,12 @@ function Contact() {
                             class="w-100 sm:h-48 lg:h-32 mt-2 py-3 px-3 sm:text-3xl lg:text-lg rounded-lg bg-gray-800 border border-gray-400 dark:border-gray-700 text-white font-semibold focus:border-cyan-400 focus:outline-none"></textarea>
                         </div>
                         {uploadStatus === 'success' && (
-                          <div className="mt-6 text-center text-green-600 text-lg font-semibold">
+                          <div className="mt-6 text-center text-green-600 sm:text-2xl lg:text-xl font-semibold">
                                Successfully send !!!
                              </div>
                              )}
                       {uploadStatus === 'error' && (
-                         <div className="mt-6 text-center text-red-600 text-lg font-semibold">
+                         <div className="mt-6 text-center text-red-600 sm:text-2xl lg:text-xl font-semibold">
                               Failed to send your message...
                          </div>
                           )}
