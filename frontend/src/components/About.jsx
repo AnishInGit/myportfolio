71b8273088d3399fc379f8b98e55aa375202d501
AnishInGit <123265800+AnishInGit@users.noreply.github.com>
2025-09-06 11:47:59 +0530

    updateA

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -16,16 +16,21 @@ const About = () => {
         <div className="border-cyan-400 border-4 m-5 text-cyan-400 hover:bg-cyan-400 hover:text-black shadow-2xl hover:shadow-[10px_5px_60px_25px_rgba(0,255,255,0.4)]  ">
         <ScrollAnimation animateIn="animate__animated animate__fadeInLeft " delay={.1 * 1000}>
           <p className="sm:text-2xl lg:text-lg leading-relaxed font-medium  pl-10 pr-5 pt-5 ">
-            Hi there! I'm Anish, and I am pursuing a B.Tech degree in Computer Science Engineering at SRM University. Throughout my academic journey, I have developed a strong foundation in data structures, web development, algorithms and more.
+            Hi there! My name is Anish Maity, aB. Tech graduate in Computer Science and Engineering from SRM University with a strong
+foundation in data structures, algorithms, and full-stack web development. Skilled in building scalable web applications
+and machine learning models.
           </p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="animate__animated animate__fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
           <p className="sm:text-2xl lg:text-lg leading-relaxed font-medium pl-10 pr-5">
-          My passion for technology drives me to excel in development roles, where I aim to create innovative and efficient solutions. </p>
+          Passionate about creating innovative solutions through technology, with hands-on
+experience in real-time systems and predictive analytics. </p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="animate__animated animate__fadeInLeft" delay={0.3 * 1000}>
           <p className="sm:text-2xl lg:text-lg leading-relaxed font-medium  pl-10 pr-5 pb-5">
-          Now, I am constantly seeking opportunities to expand my knowledge and stay updated with the latest advancements.                 I am driven to leverage my skills and experiences to make a meaningful impact in the field of computer science engineering.
+          Committed to continuous learning and eager to contribute
+effectively in a dynamic software development environment.
+
           </p>
         </ScrollAnimation>
         </div>
